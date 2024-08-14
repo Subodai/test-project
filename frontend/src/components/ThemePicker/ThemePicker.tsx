53688c96d4d286900333e23737b2e5100d1569ce
Mark Thompson <subodai85@gmail.com>
2024-08-14 23:51:42 +0100

    Added Axios, fixed top menu

--- frontend/src/components/ThemePicker/ThemePicker.tsx
+++ frontend/src/components/ThemePicker/ThemePicker.tsx
@@ -1,22 +1,25 @@
-import { ActionIcon, Group, useComputedColorScheme, useMantineColorScheme } from "@mantine/core";
+import { ActionIcon, Group, Title, useComputedColorScheme, useMantineColorScheme } from "@mantine/core";
 import { IconSun, IconMoon } from '@tabler/icons-react';
 import cx from 'clsx';
-import classes from './Demo.module.css';
+import classes from './Themepicker.module.css';
 
 export default function ThemePicker() {
   const { setColorScheme } = useMantineColorScheme();
   const computedColorScheme = useComputedColorScheme('light', { getInitialValueInEffect: true });
   return (
-    <Group>
-      <ActionIcon
-        onClick={() => setColorScheme(computedColorScheme === 'light' ? 'dark' : 'light')}
-        variant="default"
-        size="xl"
-        aria-label="Toggle Colour Scheme"
-      >
-        <IconSun className={cx(classes.icon, classes.light)} stroke={1.5} />
-        <IconMoon className={cx(classes.icon, classes.dark)} stroke={1.5} />
-      </ActionIcon>
+    <Group justify="space-between" style={{ flex: 1 }}>
+      <Title size="20">Test App</Title>
+      <Group ml="xl" gap={0} visibleFrom="sm">
+        <ActionIcon
+          onClick={() => setColorScheme(computedColorScheme === 'light' ? 'dark' : 'light')}
+          variant="default"
+          size="lg"
+          aria-label="Toggle Colour Scheme"
+        >
+          <IconSun className={cx(classes.icon, classes.light)} stroke={1.5} />
+          <IconMoon className={cx(classes.icon, classes.dark)} stroke={1.5} />
+        </ActionIcon>
+      </Group>
     </Group>
   );
 }
